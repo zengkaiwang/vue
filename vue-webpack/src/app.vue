29c12563022b2a0d7bff50bd25ca57e9c4a130e2
王增凯 <zengkai.wang@irootech.com>
2018-07-27 13:47:02 +0800

    创建src目录及页面app.vue

---
+++ vue-webpack/src/app.vue
@@ -0,0 +1,13 @@
+<template> 
+	<div class="app"> {{msg}} </div> 
+</template> 
+
+<script> 
+	export default { data(){ return { msg:'aaaaaaa' } } } 
+</script> 
+
+<style> 
+	.app{ width: 100%; height: 50px; border: 1px solid red; } 
+</style>
+
+
